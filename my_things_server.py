00a659a07ecc1b8711048f138894a8ae2255b68d
Rob <robgroves0.gmailcom>
2018-07-06 20:45:05 -0400

    fix: put checkDatabasePrerequisites into t try: except: block to handle when database changes are pending, but haven't been flask db upgraded yet

--- my_things_server.py
+++ my_things_server.py
@@ -10,14 +10,18 @@ hello_flask: First Python-Flask webapp
 """
 import os
 #from mts_app import db
-from mts_app import create_app
+from mts_app import create_app, migrate
 from mts_app import models
 from mts_app.helpers import checkDatabasePrerequisites
-from flask_migrate import Migrate
+from sqlalchemy.exc import OperationalError
 
 app=create_app(os.getenv('FLASK_CONFIG') or 'default')
 if not app.config['TESTING']:
-    with app.app_context():
-        checkDatabasePrerequisites()
+    try:
+        with app.app_context():
+            checkDatabasePrerequisites()
+    except OperationalError:
+        # we assume that flask migrate/upgrade needs to e run first
+        pass
 if __name__ == '__main__':
     app.run()
